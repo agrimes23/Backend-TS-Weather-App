095c89b1a0da753de7119323f25f50a11b5072d3
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-05-05 23:18:39 -0500

    successfully used backend to make open weather api call

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
-import express from 'express'
+const express = require('express')
 const app = express()
+const axios = require('axios')
+require('dotenv').config()
+
+const PORT = 5000
+
+const APIKEY = process.env.API_KEY
+
+app.get("/api/weather/:cityname", (req, res) => {
+
+    const options = {
+        method: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + req.params.cityname + "&appid=" + APIKEY + "&units=imperial",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+
+    axios.request(options)
+    .then(res => console.log(res.data), error => console.log(error))
+    
+})
+
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
